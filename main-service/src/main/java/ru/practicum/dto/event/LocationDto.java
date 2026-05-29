38065395ebbc16af86ca10dcc140763aedef2f71
Artush Avetisyan <Artush.Avetis@yandex.com>
2026-05-29 16:17:19 +0300

    Add lombok annotations to LocationDto

--- main-service/src/main/java/ru/practicum/dto/event/LocationDto.java
+++ main-service/src/main/java/ru/practicum/dto/event/LocationDto.java
@@ -1,5 +1,12 @@
 package ru.practicum.dto.event;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationDto {
     private Double lat;
     private Double lon;
