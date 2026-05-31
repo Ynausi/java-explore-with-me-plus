ebcb5740fef2f9fe0da9b0d1203db55471364e00
Maxim Trushkin <happyprattow2@mail.ru>
2026-05-31 23:42:38 +0300

    fix: исправить getStats() в StatsServiceImpl

--- stat/stat-server/src/main/java/ru/practicum/storage/HitRepository.java
+++ stat/stat-server/src/main/java/ru/practicum/storage/HitRepository.java
@@ -1,9 +1,6 @@
 package ru.practicum.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import ru.practicum.dto.StatsViewProjection;
 import ru.practicum.model.Hit;
 
 import java.time.LocalDateTime;
@@ -11,27 +8,8 @@ import java.util.List;
 
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
-    @Query("""
-            SELECT h.app, h.uri, COUNT(h.ip) FROM Hit h
-            WHERE h.timestamp BETWEEN :start AND :end
-            AND (:uris IS NULL OR uri IN :uris)
-            GROUP BY h.app, h.uri
-            ORDER BY COUNT(h.ip) DESC
-            """)
-    List<StatsViewProjection> findStatsViewProjections(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("uris") List<String> uris);
+    List<Hit> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
-    @Query("""
-            SELECT h.app, h.uri, COUNT(DISTINCT h.ip) FROM Hit h
-            WHERE h.timestamp BETWEEN :start AND :end
-            AND (:uris IS NULL OR uri IN :uris)
-            GROUP BY h.app, h.uri
-            ORDER BY COUNT(DISTINCT h.ip) DESC
-            """)
-    List<StatsViewProjection> findUniqueStatsViewProjections(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end,
-            @Param("uris") List<String> uris);
+    List<Hit> findByTimestampBetweenAndUriIn(LocalDateTime timestampAfter, LocalDateTime timestampBefore,
+                                             List<String> uris);
 }
